cc02fa76c4295c04b064ed79a2f5db3b597ffa35
listen2099 <listen2099@gmail.com>
2019-06-24 17:27:35 +0800

    ok

--- meta.py
+++ meta.py
@@ -1,9 +1,29 @@
 class ModelMeta(type):
     def __new__(cls, name, bases, attrs):
-        print('--------------------------')
+        print('1--------------------------')
         print(cls)
         print(name)
         print(bases)
         print(attrs)
-        print('--------------------------')
+        print('2--------------------------')
         return super().__new__(cls, name, bases, attrs)
+
+
+class Base(metaclass=ModelMeta):
+    """它自己和子类来自ModelMeta"""
+    pass
+
+
+class Field:  # 描述字段
+    def __init__(self, field_name='id', pk=False, nullable=True):
+        self.field_name = field_name
+        self.pk = pk
+        self.nullable = nullable
+
+
+#  SQLAlchemy
+#  student
+class Student(Base):
+    id = Field()
+    name = Field()
+    age = Field()
